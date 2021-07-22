c756e4eee0f0138f1ff8fbc8043ebb0077f7adc9
ha1fdaew <b3.nn1ed@aol.com>
2021-07-22 14:14:06 +0400

    Clean things up

--- src/main/java/benn1ed/curseofdisintegration/ModData.java
+++
@@ -1,8 +0,0 @@
-package benn1ed.curseofdisintegration;
-
-public class ModData
-{
-    public static final String MODID = "curseofdisintegration";
-    public static final String NAME = "Curse Of Disintegration";
-    public static final String VERSION = "1.0.0";
-}
